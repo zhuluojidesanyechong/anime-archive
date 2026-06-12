ad63ed45eb099767734d0c0ce4313cb86b2e1484
zhuluojidesanyechong <zhuluojidesanyechong@gmail.com>
2026-06-12 11:55:02 +0800

    上传文件

---
+++ data.js
@@ -0,0 +1,116 @@
+const works = {
+
+  madoka: {
+    title: "魔法少女小圆",
+
+    albums: [
+  {
+    title: "madoka_001",
+    images: [
+      "images/madoka_001.jpg",
+      "images/madoka_001_hd.jpg"
+    ]
+  },
+
+  {
+    title: "madoka_002",
+    images: [
+      "images/madoka_002.jpg",
+      "images/madoka_002_hd.jpg"
+    ]
+  },
+
+  {
+    title: "madoka_003",
+    images: [
+      "images/madoka_003.jpg",
+      "images/madoka_003_hd.jpg"
+    ]
+  }
+]
+ },
+ railgun: {
+  title: "某科学的超电磁炮",
+
+  albums: [
+    {
+      title: "railgun_001",
+      images: [
+        "images/railgun_001.jpg",
+        "images/railgun_001_hd.jpg"
+      ]
+    },
+
+    {
+      title: "railgun_002",
+      images: [
+        "images/railgun_002.jpg",
+        "images/railgun_002_hd.jpg"
+      ]
+    },
+
+   {
+      title: "railgun_003",
+      images: [
+        "images/railgun_003.jpg",
+        "images/railgun_003_hd.jpg"
+      ]
+    }
+  ]
+},
+  maidragon: {
+    title: "小林家的龙女仆",
+
+    albums: [
+      {
+        title: "maidragon_001",
+        images: [
+          "images/maidragon_001.jpg",
+          "images/maidragon_001_hd.jpg"
+        ]
+      }
+    ]
+  },
+
+  onimai: {
+    title: "不当欧尼酱了",
+
+    albums: [
+      {
+        title: "onimai_001",
+        images: [
+          "images/onimai_001.jpg",
+          "images/onimai_001_hd.jpg"
+        ]
+      }
+    ]
+  },
+
+  wataten: {
+    title: "天使降临到我身边",
+
+    albums: [
+      {
+        title: "wataten_001",
+        images: [
+          "images/wataten_001.jpg",
+          "images/wataten_001_hd.jpg"
+        ]
+      }
+    ]
+  },
+
+k_on: {
+  title: "轻音少女",
+
+  albums: [
+    {
+      title: "k_on_001",
+      images: [
+        "images/k_on_001.jpg",
+        "images/k_on_001_hd.jpg"
+      ]
+    }
+  ]
+}
+};
